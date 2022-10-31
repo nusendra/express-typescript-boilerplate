c14161f0a917509a1390d8cdcae7702c049d6b23
nusendra <admin@nusendra.com>
2022-10-31 11:41:50 +0700

    feat: add husky

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port: string = process.env.PORT || "3000";
+const port: string = process.env.PORT ?? "3000";
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
